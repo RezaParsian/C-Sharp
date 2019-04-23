9027c2e186fb20a2adeb08f0ff43bb632c586b19
Reza_Atom <46934941+RezaParsian@users.noreply.github.com>
2019-04-23 10:56:30 +0430

    Add files via upload

---
+++ E-Namad/E-Namad/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace E_Namad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(380, 275);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Fetch Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 334);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(398, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(69, 17);
+            this.toolStripStatusLabel1.Text = "Rp-Enamad";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(93, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Save(All)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Margin = new System.Windows.Forms.Padding(120, 3, 0, 2);
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 17);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(174, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Save(Url)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 356);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rp-Enamad";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ E-Namad/E-Namad/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace E_Namad
+{
+    public partial class Form1 : Form
+    {
+        public string cookie = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void doit(int offset,int limit=100)
+        {
+            toolStripStatusLabel1.Text = "Fetching Data : ";
+            StreamWriter st = new StreamWriter(@Path.GetTempPath().ToString()+"result.json");
+            toolStripProgressBar1.Maximum = offset;
+            for (int x = 0; x <= offset; x += limit)
+            {
+                toolStripProgressBar1.Value = x;
+                string url = $"http://enamad.ir/Home/DomainListDataForMIMT?order=asc&offset={x}&limit={limit}";
+                WebClient wb = new WebClient();
+                wb.Headers[HttpRequestHeader.Cookie] = cookie;
+                wb.Headers[HttpRequestHeader.ContentType] = "application/json; charset=utf-8";
+                string result = wb.DownloadString(url);
+
+                JObject Json = JObject.Parse(result);
+
+                if (Json.ToString().Contains("\"rows\": []"))
+                {
+                    st.WriteLine(Json);
+                    break;
+                }
+                else
+                {
+                    st.WriteLine(Json);
+                }
+            }
+            st.Close();
+            toolStripProgressBar1.Maximum = toolStripProgressBar1.Value;
+
+            StreamReader rd = new StreamReader(@Path.GetTempPath().ToString() + "result.json");
+            textBox1.Text = rd.ReadToEnd();
+            rd.Close();
+
+            //Remove something
+            textBox1.Text = textBox1.Text.Replace("      \"cycleStatus\": null\r\n", "");
+            textBox1.Text = textBox1.Text.Replace("     \"postalcode\": null,\r\n", "");
+            textBox1.Text = textBox1.Text.Replace("     \"address\": null,\r\n", "");
+            textBox1.Text = textBox1.Text.Replace("     \"mobile\": null,\r\n", "");
+            textBox1.Text = textBox1.Text.Replace("     \"name\": null,\r\n", "");
+            textBox1.Text = textBox1.Text.Replace("     \"nationalId\": null,\r\n", "");
+            textBox1.Text = textBox1.Text.Replace("     \"datetime\": null,\r\n", "");
+            textBox1.Text = textBox1.Text.Replace("     \"phone\": null,\r\n", "");
+            textBox1.Text = textBox1.Text.Replace("     \"datetimeop\": null,\r\n", "");
+
+            toolStripStatusLabel1.Text = "Progress is finished -> ";
+
+            button2.Enabled = true;
+            button3.Enabled = true;
+        }
+        void saveurl()
+        {
+            toolStripStatusLabel1.Text = "Spliting url : ";
+
+            MatchCollection m = Regex.Matches(textBox1.Text, "\"domain\": \"(.*?)\"");
+
+            StreamWriter sw = new StreamWriter("Rp-Enamad(Url).txt");
+
+            toolStripProgressBar1.Maximum = m.Count;
+            for (int x = 0; x < m.Count; x++)
+            {
+                toolStripProgressBar1.Value = x;
+                sw.WriteLine(m[x].Groups[1].Value);
+            }
+            sw.Close();
+            toolStripStatusLabel1.Text = "Progress is finished -> ";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(Environment.ExitCode);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            int total;
+
+            //Get New Cookie
+            string url = "http://enamad.ir";
+            using (WebClient wb = new WebClient())
+            {
+                wb.OpenRead(url);
+                cookie = wb.ResponseHeaders["Set-Cookie"];
+            }
+            int c = cookie.IndexOf("cookiesession1");
+            int v = cookie.LastIndexOf(";Path=");
+            cookie = cookie.Substring(c, v - c);
+
+            //Get Totat Domain Address Fast!!
+            url = "http://enamad.ir/Home/DomainListDataForMIMT?order=asc&offset=0&limit=1";
+            using (WebClient wb = new WebClient())
+            {
+                wb.Headers[HttpRequestHeader.Cookie] = cookie;
+                wb.Headers[HttpRequestHeader.ContentType] = "application/json; charset=utf-8";
+                string result = wb.DownloadString(url);
+
+                JObject Json = JObject.Parse(result);
+
+                total = int.Parse(Json["total"].ToString());
+            }
+
+            Thread t = new Thread(() => doit(total));
+            t.Start();
+            MessageBox.Show("It May Take a While","Info",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamWriter sr = new StreamWriter("Rp-Enamad.txt");
+            sr.WriteLine(textBox1.Text);
+            sr.Close();
+            toolStripStatusLabel1.Text = "Saved.";
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel2.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            button1.Enabled = true;
+            Thread t = new Thread(saveurl);
+            t.Start();
+            MessageBox.Show("It May Take a While", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
